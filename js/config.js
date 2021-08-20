2c9b166ad610b2af5d335c95c012a1e8f61c9fb8
jshubo888 <jshubo@gmail.com>
2021-08-20 14:31:18 +1000

    updated config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,12 @@
 window._config = {
+	/*
     cognito: {
         userPoolId: 'eu-west-1_JTi25cDVB',
         userPoolClientId: '5qi4adsulhj6c35qgngict3car',
         region: 'eu-west-1'
     },
+	*/
     api: {
-        invokeUrl: 'https://k2cdp5ols3.execute-api.eu-west-1.amazonaws.com/prod'
+        invokeUrl: 'https://nd4hb7zoq8.execute-api.us-east-1.amazonaws.com/dev'
     }
 };
